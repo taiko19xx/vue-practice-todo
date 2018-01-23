8d13ce324920de5c6f7b148fa94d5d6aa80835c4
taiko19xx <hello@taiko19xx.net>
2018-01-23 23:33:12 +0900

    ファイルコンポーネント化

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="app">
+      <input v-model="newtask" v-on:keyup.enter="add">
+      <ul>
+        <li v-for="(item, index) in items">
+          <span v-if="item.completed"><a v-on:click="click(index)"><del>{{ item.task }}</del></a></span>
+          <span v-else><a v-on:click="click(index)">{{ item.task }}</a></span>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data () {
+    return {
+      newtask: '',
+      items: [
+        {task: "doing", completed: false},
+        {task: "completed", completed: true}
+      ]
+    }
+  },
+  methods: {
+    add: function (event) {
+      this.items.push({task: this.newtask, completed: false})
+      this.newtask = ''
+    },
+    click: function (index) {
+      // this.item.completed = !this.item.completed
+      let item = this.items[index]
+      item.completed = !item.completed
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
